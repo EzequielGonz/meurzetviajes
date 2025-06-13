d383324f508af106aed6c7ebcb6428819ce6ba7d
Ezequiel <ezegonza724@gmail.com>
2025-06-13 18:56:53 -0300

    Cambios de las fotos y agregacion del boton en la red de meurzet

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./catalogo.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
